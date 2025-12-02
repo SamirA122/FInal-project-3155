64f365a706e8cbda25a0048a95ee7b0c5ee163d9
Yousef Ayesh <ayesh4002@gmail.com>
2025-12-02 13:30:54 -0500

    Removed password

--- FinalProject/api/dependencies/config.py
+++ FinalProject/api/dependencies/config.py
@@ -3,6 +3,6 @@ class conf:
     db_name = "YousefSamirDB"
     db_port = 3306
     db_user = "root"
-    db_password = "mamawebaba2"
+    db_password = "***"
     app_host = "localhost"
     app_port = 8000
